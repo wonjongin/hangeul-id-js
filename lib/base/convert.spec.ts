b2b4daca396bef9356754944be54e54e94aabfca
wonjongin <wji0227@naver.com>
2022-01-21 18:20:03 +0900

    ADD: convert base functions

--- lib/base/convert.spec.ts
+++ lib/base/convert.spec.ts
@@ -1,4 +1,8 @@
-import { convert_base_from_dec } from "./convert";
+import {
+  convert_base,
+  convert_base_from_dec,
+  convert_base_to_dec,
+} from "./convert";
 import { expect } from "chai";
 
 describe("test: convert", () => {
@@ -7,5 +11,19 @@ describe("test: convert", () => {
     let res2 = convert_base_from_dec(2350, 2350);
     expect(res1).to.deep.equal([1, 2, 2349]);
     expect(res2).to.deep.equal([1, 0]);
+  });
+
+  it("test: convert_base_to_dec", () => {
+    let res1 = convert_base_to_dec([1, 2, 2349], 2350);
+    let res2 = convert_base_to_dec([1, 0], 2350);
+    expect(res1).to.deep.equal(5529549);
+    expect(res2).to.deep.equal(2350);
+  });
+
+  it("test: convert_base", () => {
+    let res1 = convert_base([1, 2, 2349], 2350, 16);
+    let res2 = convert_base([1, 0], 2350, 2);
+    expect(res1).to.deep.equal([5, 4, 5, 15, 12, 13]);
+    expect(res2).to.deep.equal([1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0]);
   });
 });
--- lib/base/convert.ts
+++ lib/base/convert.ts
@@ -20,3 +20,21 @@ export function convert_base_from_dec(num: number, to: number): Array<number> {
   res.push(mid_int);
   return res;
 }
+
+export function convert_base_to_dec(num: Array<number>, from: number): number {
+  let res: number = 0;
+  num.map((e, i, a) => {
+    res += e * Math.pow(from, a.length - 1 - i);
+  });
+  return res;
+}
+
+export function convert_base(
+  num: Array<number>,
+  from: number,
+  to: number
+): Array<number> {
+  let mid = convert_base_to_dec(num, from);
+  let res = convert_base_from_dec(mid, to);
+  return res;
+}
